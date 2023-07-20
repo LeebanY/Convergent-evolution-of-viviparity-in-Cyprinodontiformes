a67c57d3964c2f732c99a6fb7b3ebc1c4a0a5ab9
Leeban Yusuf <34740965+LeebanY@users.noreply.github.com>
2023-07-20 15:56:14 +0100

    Update Run_MSA_to_Alignment.sh

--- MultipleSequenceAlignment/Run_MSA_to_Alignment.sh
+++ MultipleSequenceAlignment/Run_MSA_to_Alignment.sh
@@ -7,4 +7,9 @@
 #$ -m e
 #$ -M ly36@st-andrews.ac.uk
 
-./maffilter param=
+#Use maf filter to extract genes from multiple sequence alignment.
+
+./maffilter param=MSAalignment_to_PerGeneAlignment.bpp
+
+
+# Maf filter citation: Dutheil, J.Y., Gaillard, S. and Stukenbrock, E.H., 2014. MafFilter: a highly flexible and extensible multiple genome alignment files processor. BMC genomics, 15, pp.1-10.
